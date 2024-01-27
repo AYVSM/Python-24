4c0aff74a222b3c886c51f22d6af9d88e4873fcc
AYVSM <ayvsm76@gmail.com>
2024-01-27 09:36:14 +0700

    Python

---
+++ MEET0/Biodata.py
@@ -0,0 +1,9 @@
+nama = "Albert"
+umur = 15 
+tinggi =  169.5
+status = True
+
+print( "Nama = " , nama, " Tipe Data = ", type(nama) )
+print( "Umur = " , umur, " Tipe Data = ", type(umur)  )
+print( "Tinggi = " , tinggi, " Tipe Data = ", type(tinggi)  )
+print( "Status = " , status, " Tipe Data = ", type(status)  )
