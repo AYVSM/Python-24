1432fed016b7ea88a8d6ee9a611646f11fef5e25
AYVSM <ayvsm76@gmail.com>
2024-02-17 11:40:22 +0700

    17-02-2024

--- MEET10/Latihan_10.py
+++ MEET10/Latihan_10.py
@@ -1,8 +1,25 @@
-for i in range(1, 6):  
-    print(" " * (5 - i) + "*" * (2 * i - 1))
+print("Belah ketupat")
+i = 0
+count = 20
+space = int(1/2 * count) - 1
+while True:
+    if i == count:
+        break
+    elif i % 2:
+        print(f" "*space, "+"*i)
+        space -= 1
+    i += 1
 
-for i in range(4, 0, -1):  
-    print(" " * (5 - i) + "*" * (2 * i - 1))
+i = 18
+count = 0
+space = int(1/2 * count) + 1
+while True:
+    if i == count:
+        break
+    elif i % 2:
+        print(f" "*space, "+"*i)
+        space += 1
+    i -= 1
 
 print("kupu-kupu :")
 for i in range(1, 6):
